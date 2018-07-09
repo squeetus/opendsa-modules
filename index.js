75950001bdf283e1c4c1097b558d9709f722ea4d
squeetus <burlinsond2@gmail.com>
2018-07-09 17:06:05 -0400

    Adding script to update submodules and prepare environment

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').createServer(app);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/textbook'));
+app.use(express.static(__dirname + '/public/textbook/RST'));
+app.use(express.static(__dirname + '/public/textbook/lib'));
+app.use(express.static(__dirname + '/public/textbook/Books/simple_demo/html/'));
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join( __dirname + '/public/textbook/Books/simple_demo/html/index.html'));
+});
+
+server.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+exports = module.exports = app;
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Bash script to prepare textbook environment
+
+git submodule update --init --recursive --remote
+
+cd ./public/textbook/JSAV
+make
+
+cd ./../
+make min
